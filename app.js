cef3f2acba6cb857b0b8913da2efbd3431c5dfbe
Aditya Kumar Jha <adityajha126@gmail.com>
2019-06-22 03:23:21 +0530

    aecond commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Add Book To List
+UI.prototype.addBookToList = function(book){
+  const list = document.getElementById('book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X<a></td>
+  `;
+
+  list.appendChild(row);
+}
+
+// Clear Fields
+UI.prototype.clearFields = function() {
+  document.getElementById('title').value = '';
+  document.getElementById('author').value = '';
+  document.getElementById('isbn').value = '';
+}
+
+// Event Listeners
+document.getElementById('book-form').addEventListener('submit', function(e){
+  // Get form values
+  const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        isbn = document.getElementById('isbn').value
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+ 
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
